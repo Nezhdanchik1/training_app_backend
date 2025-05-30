9235f5a5a172c582f486c996d2c2b3e0ba2008ad
Nezhdanchik1 <myktybekov1234@gmail.com>
2025-05-30 13:58:34 +0500

    env added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
@@ -18,7 +19,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/exercises', exerciseRoutes);
 app.use('/api/plans', workoutPlanRoutes);
 
-mongoose.connect('mongodb://localhost:27017/training_app_db')
+mongoose.connect(process.env.MONGODB_CONNECT_URI)
 mongoose.connection.on('connected', () => {
   console.log("DB connected");
 });
